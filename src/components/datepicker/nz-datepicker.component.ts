c3c4ab8c33d30d8f1c7eb9992414e6a394ce1cfb
tqcenglish <tqcenglish@gmail.com>
2017-10-10 15:14:39 +0800

    add zh-CN and en-US

--- src/components/datepicker/nz-datepicker.component.ts
+++ src/components/datepicker/nz-datepicker.component.ts
@@ -15,6 +15,9 @@ import { NzTimePickerInnerComponent } from '../time-picker/nz-timepicker-inner.c
 import { DEFAULT_DATEPICKER_POSITIONS } from '../core/overlay/overlay-position-map';
 import { ConnectionPositionPair } from '../core/overlay';
 
+import * as zh from './zh-CN.json';
+import * as en from './en-US.json';
+
 @Component({
   selector     : 'nz-datepicker',
   encapsulation: ViewEncapsulation.None,
@@ -63,36 +66,36 @@ import { ConnectionPositionPair } from '../core/overlay';
                 #dateBox
                 (blur)="_blurInput(dateBox)">
             </div>
-            <a class="ant-calendar-clear-btn" title="清除"></a>
+            <a class="ant-calendar-clear-btn" title="languages['Clear']"></a>
           </div>
           <div class="ant-calendar-date-panel">
             <div class="ant-calendar-header">
               <div style="position: relative;" *ngIf="_mode!='time'">
-                <a class="ant-calendar-prev-year-btn" title="上一年" (click)="_preYear()"></a>
-                <a class="ant-calendar-prev-month-btn" title="上个月" (click)="_preMonth()"></a>
+                <a class="ant-calendar-prev-year-btn" title="language['Pre Year']" (click)="_preYear()"></a>
+                <a class="ant-calendar-prev-month-btn" title="language['Pre Month]'" (click)="_preMonth()"></a>
                 <span class="ant-calendar-ym-select">
-                <a class="ant-calendar-month-select" title="选择月份" (click)="_changeMonthView()">{{_showMonth + 1}}月</a>
-                <a class="ant-calendar-year-select" title="选择年份" (click)="_changeDecadeView($event)">{{_showYear}}年</a>
+                <a class="ant-calendar-month-select" title="languages['Choice Month']" (click)="_changeMonthView()">{{_showMonth + 1}}月</a>
+                <a class="ant-calendar-year-select" title="languages['Choice Year']" (click)="_changeDecadeView($event)">{{_showYear}}年</a>
                 </span>
-                <a class="ant-calendar-next-month-btn" title="下个月" (click)="_nextMonth()"></a>
-                <a class="ant-calendar-next-year-btn" title="下一年" (click)="_nextYear()"></a>
+                <a class="ant-calendar-next-month-btn" title="languages['Next Month']" (click)="_nextMonth()"></a>
+                <a class="ant-calendar-next-year-btn" title="languages['Next Year']" (click)="_nextYear()"></a>
               </div>
               <div style="position: relative;" *ngIf="_mode=='time'">
                 <span class="ant-calendar-my-select">
                   <a class="ant-calendar-year-select" title="Choose a month">{{_selectedYear}}年</a>
                   <a class="ant-calendar-month-select" title="Choose a month">{{_showMonth + 1}}月</a>
-                  <a class="ant-calendar-day-select">{{_selectedDate}}日</a>
+                  <a class="ant-calendar-day-select">{{_selectedDate}}{{languages['Day']}}</a>
                 </span>
               </div>
               <div class="ant-calendar-month-panel" *ngIf="_mode=='month'">
                 <div>
                   <div class="ant-calendar-month-panel-header">
-                    <a class="ant-calendar-month-panel-prev-year-btn" title="上一年" (click)="_preYear()"></a>
-                    <a class="ant-calendar-month-panel-year-select" title="选择年份" (click)="_changeDecadeView($event)">
+                    <a class="ant-calendar-month-panel-prev-year-btn" title="language['Pre Year']" (click)="_preYear()"></a>
+                    <a class="ant-calendar-month-panel-year-select" title="languages['Choice Year']" (click)="_changeDecadeView($event)">
                       <span class="ant-calendar-month-panel-year-select-content">{{_showYear}}</span>
                       <span class="ant-calendar-month-panel-year-select-arrow">x</span>
                     </a>
-                    <a class="ant-calendar-month-panel-next-year-btn" title="下一年" (click)="_nextYear()"></a>
+                    <a class="ant-calendar-month-panel-next-year-btn" title="languages['Next Year']" (click)="_nextYear()"></a>
                   </div>
                   <div class="ant-calendar-month-panel-body">
                     <nz-calendar
@@ -114,12 +117,12 @@ import { ConnectionPositionPair } from '../core/overlay';
               <div class="ant-calendar-year-panel" *ngIf="_mode=='decade'">
                 <div>
                   <div class="ant-calendar-year-panel-header">
-                    <a class="ant-calendar-year-panel-prev-decade-btn" title="上一年代" (click)="_preDecade()"></a>
-                    <a class="ant-calendar-year-panel-decade-select" title="选择年代">
+                    <a class="ant-calendar-year-panel-prev-decade-btn" title="languages['Pre Decade']" (click)="_preDecade()"></a>
+                    <a class="ant-calendar-year-panel-decade-select" title="languages['Choice Decade']">
                       <span class="ant-calendar-year-panel-decade-select-content">{{_startDecade}}-{{_startDecade + 9}}</span>
                       <span class="ant-calendar-year-panel-decade-select-arrow">x</span>
                     </a>
-                    <a class="ant-calendar-year-panel-next-decade-btn" title="下一年代" (click)="_nextDecade()"></a>
+                    <a class="ant-calendar-year-panel-next-decade-btn" title="languages['Next Decade']" (click)="_nextDecade()"></a>
                   </div>
                   <div class="ant-calendar-year-panel-body">
                     <table class="ant-calendar-year-panel-table" cellspacing="0" role="grid">
@@ -158,10 +161,10 @@ import { ConnectionPositionPair } from '../core/overlay';
             </div>
             <div class="ant-calendar-footer ant-calendar-footer-show-ok">
                 <span class="ant-calendar-footer-btn">
-                  <a class="ant-calendar-today-btn " [attr.title]="_today|nzDate:nzFormat" (click)="_changeToToday()">{{nzShowTime ? '此刻' : '今天'}}</a>
-                  <a class="ant-calendar-time-picker-btn" (click)="_changeTimeView($event)" *ngIf="(_mode != 'time')&&nzShowTime">选择时间</a>
-                  <a class="ant-calendar-time-picker-btn" (click)="_changeYearView($event)" *ngIf="(_mode == 'time')&&nzShowTime">选择日期</a>
-                  <a class="ant-calendar-ok-btn" *ngIf="nzShowTime" (click)="_closeCalendar()">确 定</a>
+                  <a class="ant-calendar-today-btn " [attr.title]="_today|nzDate:nzFormat" (click)="_changeToToday()">{{nzShowTime ? languages['Now'] : languages['Today']}}</a>
+                  <a class="ant-calendar-time-picker-btn" (click)="_changeTimeView($event)" *ngIf="(_mode != 'time')&&nzShowTime">{{languages['Choice Time']}}</a>
+                  <a class="ant-calendar-time-picker-btn" (click)="_changeYearView($event)" *ngIf="(_mode == 'time')&&nzShowTime">{{languages['Choice Date']}}</a>
+                  <a class="ant-calendar-ok-btn" *ngIf="nzShowTime" (click)="_closeCalendar()">{{languages['Confirm']}}</a>
                 </span>
             </div>
           </div>
@@ -209,7 +212,7 @@ export class NzDatePickerComponent implements ControlValueAccessor, OnInit {
   @ViewChild('trigger') trigger;
   @ViewChild(NzTimePickerInnerComponent) timePickerInner: NzTimePickerInnerComponent;
   @HostBinding('class.ant-calendar-picker') _nzCalendarPicker = true;
-
+  languages = {};
   @Input()
   get nzDisabled(): boolean {
     return this._disabled;
@@ -397,6 +400,11 @@ export class NzDatePickerComponent implements ControlValueAccessor, OnInit {
 
   ngOnInit() {
     this._generateYearPanel();
+    if(localStorage.getItem('language') === 'zh-CN'){
+      this.languages =  zh;
+    }else{
+      this.languages = en;
+    }
   }
 
   writeValue(value: any): void {
